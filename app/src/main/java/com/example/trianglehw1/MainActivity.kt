170b4831c35dfde12f53b1279d462d3bede0b899
glover-sd <brent.l.glover@gmail.com>
2021-09-10 13:28:47 -0700

    Working navigation between fragments. Updated startup page with purpose and usage text.

--- app/src/main/java/com/example/trianglehw1/MainActivity.kt
+++ app/src/main/java/com/example/trianglehw1/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.trianglehw1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
 import com.example.trianglehw1.fragments.donate_fragment
 import com.example.trianglehw1.fragments.startup_fragment
 import com.example.trianglehw1.fragments.triangle_fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,32 +16,37 @@ class MainActivity : AppCompatActivity() {
     private val donateFragment: donate_fragment = donate_fragment()
     private val triangleFragment: triangle_fragment = triangle_fragment()
 
-    //  changed
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-
-        when(item.itemId){
-
-            R.id.ic_back_nav_button -> transaction.replace(R.id.fragment_container, startupFragment)
-            R.id.ic_reset_screen_button -> transaction.replace(R.id.fragment_container, triangleFragment)
-            R.id.ic_donate_nav_button -> transaction.replace(R.id.fragment_container, donateFragment)
-        }
-        transaction.commit()
-
-        true
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
+        setFragment(startupFragment)
+        view_BottomNavBar.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_startup_nav_button -> setFragment(startupFragment)
+                R.id.ic_triangle_nav_button -> setFragment(triangleFragment)
+                R.id.ic_donate_nav_button -> setFragment(donateFragment)
+                R.id.ic_reset_screen_button -> resetScreen()
+            }
+            true
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_container,  startupFragment)
+
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+        transaction.setReorderingAllowed(true)
+        transaction.replace(R.id.fragment_container,  fragment)
         transaction.commit()
 
-        // TEST COMMENT
+    }
+
+    private fun resetScreen() {
+
+        //  TODO: insert call to current fragment to reset data to defaults
     }
 }
+
