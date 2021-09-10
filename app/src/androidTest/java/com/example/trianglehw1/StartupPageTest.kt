7b26e7494a8c235a2065d1ef44614f60f3d8b3a8
Cmora2411 <cmsoftwareanalyst@gmail.com>
2021-09-09 23:23:28 -0700

    Given that TriangleApp is launched When the user is on yet initial startup screen Then the TriangleApp will display a purpose and usage section

--- app/src/androidTest/java/com/example/trianglehw1/StartupPageTest.kt
+++ app/src/androidTest/java/com/example/trianglehw1/StartupPageTest.kt
@@ -1,7 +1,9 @@
 package com.example.trianglehw1
 
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
@@ -21,26 +23,40 @@ class StartupPageTest {
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun startupPageTest() {
-        val textView = onView(
-            allOf(
-                withId(R.id.label_AppPurposeUsage),
-                withParent(withParent(withId(R.id.fragment_container))),
-                isDisplayed()
-            )
-        )
-        textView.check(matches(withText("Purpose and Usage of app")))
+    fun StartupPageShowsInstructions() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        //Setup
+        val textView = onView(withId(R.id.triangle))
 
-        //TODO: We can do further tests below if we want to
-        /*val textView2 = onView(
-            allOf(
-                withId(R.id.triangle), withText("startup fragment"),
-                withParent(withParent(withId(R.id.fragment_container))),
-                isDisplayed()
-            )
-        )
+        //Verify
+        textView.check(matches(withSubstring("Purpose:")))
+        textView.check(matches(withSubstring("The Triangle App will display the different types of triangles that can be created depending on the inputted triangle side lengths:")))
+        textView.check(matches(withSubstring("Scalene")))
+        textView.check(matches(withSubstring("No sides of equal lengths")))
+        textView.check(matches(withSubstring("No equal angles")))
+        textView.check(matches(withSubstring("No lines of symmetry")))
+        textView.check(matches(withSubstring("Isosceles")))
+        textView.check(matches(withSubstring("Two sides of equal lengths")))
+        textView.check(matches(withSubstring("Two equal angles")))
+        textView.check(matches(withSubstring("One line of symmetry")))
+        textView.check(matches(withSubstring("Scalene")))
+        textView.check(matches(withSubstring("Two sides of equal lengths")))
+        textView.check(matches(withSubstring("Two equal angles")))
+        textView.check(matches(withSubstring("One line of symmetry")))
+        textView.check(matches(withSubstring("Equilateral")))
+        textView.check(matches(withSubstring("Three sides of equal lengths")))
+        textView.check(matches(withSubstring("Three angles, all equal to 60°")))
+        textView.check(matches(withSubstring("Three lines of symmetry")))
+        textView.check(matches(withSubstring("Equilateral")))
+        textView.check(matches(withSubstring("Equilateral")))
+        textView.check(matches(withSubstring("Equilateral")))
+        textView.check(matches(withSubstring("Usage:")))
+        textView.check(matches(withSubstring("Input Values - The user will be able to input values  between 0.1 and 100.")))
+        textView.check(matches(withSubstring("Navigation Bar - Will allow users to refresh, navigate between the screens, and contains the Donation Button.")))
+        textView.check(matches(withSubstring("Clear Button - Will allow users to erase previous inputs and outputs.")))
+        textView.check(matches(withSubstring("Display Triangle Button - Will display the type of triangle and an image of the triangle in the output window.")))
+        textView.check(matches(withSubstring("Donation Button - Will redirect users to the donation screen where users can input their information to donate.")))
+        textView.check(matches(withSubstring("Exit Button - Will terminate the TriangleApp")))
 
-        textView2.check(matches(withText("TEST_OUR_PURPOSE_AND_USAGE_TEXT")))
-        */
     }
 }
