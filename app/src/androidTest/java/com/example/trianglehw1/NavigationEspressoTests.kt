5a8dcc856a2c4a0cd0f6ee7cb1f5974cacdd878e
Cmora2411 <cmsoftwareanalyst@gmail.com>
2021-09-11 15:10:47 -0700

    Added start button navigation test

--- app/src/androidTest/java/com/example/trianglehw1/NavigationEspressoTests.kt
+++ app/src/androidTest/java/com/example/trianglehw1/NavigationEspressoTests.kt
@@ -26,6 +26,17 @@ class NavigationEspressoTests {
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
+    fun startButtonNavigatesToTriangleFragment() {
+        //Setup
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val triangleButton = onView(withId(R.id.btn_Start))
+        triangleButton.perform(click())
+
+        //Verify
+        onView(withId(R.id.triangle_fragment)).check(matches(isDisplayed()))
+    }
+
+    @Test
     fun triangleButtonNavigatesToTriangleFragment() {
         //Setup
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
